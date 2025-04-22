3a50369be5dcfca5f239a8ad82a6c75d18dca537
ACT-STUDENT <ACT-STUDENT@LAB3-PC13>
2025-04-23 07:58:18 +0800

    Search

---
+++ Menu/Controllers/MenuController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Menu.Controllers
+{
+    public class MenuController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Get list(dishes) from databse
+            return View(/*for displaying of dishes*/);
+        }
+
+        public IActionResult Index(string Search)
+        {
+            //Get filtered list(dishes) from databse using Search
+            return View(/*for displaying of dishes*/);
+        }
+    }
+}
